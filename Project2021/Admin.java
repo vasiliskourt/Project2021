25bc891cdc5bdcb49a037ca8044b70c15f0eeb69
vasiliskourt <84717076+vasiliskourt@users.noreply.github.com>
2021-06-07 14:27:52 +0300

    Delete Project2021 directory

--- Project2021/Admin.java
+++
@@ -1,8 +0,0 @@
-public class Admin extends User{
-    private boolean isAdmin=true;
-    public Admin(String name,String phone,boolean isAdmin){
-        super(name,phone);
-        this.isAdmin=isAdmin;
-    }
-    public boolean getAdmin(){return isAdmin;}
-}
--- Project2021/Service.java
+++
@@ -1,8 +0,0 @@
-public class Service extends Entity{
-
-    public Service(String name, String description, int id){
-        super(name, description, id);
-    }
-
-    public String getDetails(){return "This object is Service.";}
-}
